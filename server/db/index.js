00cc8f6c67ebb3580b50dfb63dc99d48acc599d5
krishna chaitanya <arekapudikrishnachaitanya@gmail.com>
2024-11-29 12:47:34 +0530

    complete

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,16 @@
 import pg from "pg";
 import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const { Pool } = pg;
 const pool = new Pool({
-  user: "postgres",
-  host: "questionably-pro-gar.data-1.use1.tembo.io",
-  database: "postgres",
-  password: "exzHbGvQ8uQIxwCx",
-  port: 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGNAME,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
   ssl: {
     rejectUnauthorized: true,
     ca: fs.readFileSync("./certs/Tembo-Instance-Certificate.crt").toString(),
